33f5e72d9d009bc9a53b500f9812f571a87f70b3
helloabdul <contact@helloabdul.com>
2023-06-03 13:23:19 +0500

    feature/backend - Updating package.json files and node.

---
+++ crm/vite.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig(({ command, mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    // vite config
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': './src',
+      },
+    },
+    define: {
+      __APP_ENV__: env.APP_ENV,
+      'process.env': {
+        BACKEND_URL: 'http://localhost:50001',
+      },
+    },
+  };
+});
