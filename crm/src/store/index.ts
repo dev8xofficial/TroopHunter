766a39e3d96da4fb25d4174b53d14e4f66162047
helloabdul <contact@helloabdul.com>
2023-07-22 11:59:36 +0500

    feature/crm - Added localforage and redux-persist into project to use IndexedDB. store/persistConfig.ts has to be removed yet.

--- crm/src/store/index.ts
+++ crm/src/store/index.ts
@@ -1,10 +1,25 @@
 import { configureStore } from '@reduxjs/toolkit';
 import createSagaMiddleware from 'redux-saga';
-import { persistStore } from 'redux-persist';
-import persistedReducer from './persistConfig';
+import { persistStore, persistReducer } from 'redux-persist';
+import rootReducer from './rootReducer';
 import rootSaga from './rootSaga';
+import localforage from 'localforage';
 
 const sagaMiddleware = createSagaMiddleware();
+
+localforage.config({
+  driver: localforage.INDEXEDDB,
+  name: 'crm_database',
+  version: 1,
+  storeName: 'data_store',
+});
+
+const persistConfig = {
+  key: 'root', // Key to define the root of your state object in localforage
+  storage: localforage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = configureStore({
   reducer: persistedReducer,
@@ -14,5 +29,4 @@ const store = configureStore({
 sagaMiddleware.run(rootSaga);
 
 const persistor = persistStore(store);
-
 export { store, persistor };
