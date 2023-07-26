6d181899e4e73b457a3e0112a8614a76cba996fb
helloabdul <contact@helloabdul.com>
2023-07-26 14:14:34 +0500

    feature/crm - Rolling back from IndexedDB to Local Storage.

--- crm/src/store/index.ts
+++ crm/src/store/index.ts
@@ -1,44 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import createSagaMiddleware from 'redux-saga';
-import { persistStore, persistReducer, Transform } from 'redux-persist';
-import rootReducer from './rootReducer';
+import { persistStore } from 'redux-persist';
+import persistedReducer from './persistConfig';
 import rootSaga from './rootSaga';
-import localforage from 'localforage';
-import CryptoJS from 'crypto-js';
 
 const sagaMiddleware = createSagaMiddleware();
-
-localforage.config({
-  driver: localforage.INDEXEDDB,
-  name: 'crm_database',
-  version: 1,
-  storeName: 'data_store',
-});
-
-// Get the encryption key from the environment variable
-const encryptionKey = process.env.ENCRYPTION_KEY;
-
-const encryptTransform: Transform<any, any, any, any> = {
-  // Encrypt data before saving it to IndexedDB
-  in: (data) => {
-    const encryptedData = CryptoJS.AES.encrypt(JSON.stringify(data), encryptionKey).toString();
-    return encryptedData;
-  },
-  // Decrypt data when retrieving it from IndexedDB
-  out: (encryptedData) => {
-    const bytes = CryptoJS.AES.decrypt(encryptedData, encryptionKey);
-    const decryptedData = JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
-    return decryptedData;
-  },
-};
-
-const persistConfig = {
-  key: 'root',
-  storage: localforage,
-  transforms: [encryptTransform],
-};
-
-const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = configureStore({
   reducer: persistedReducer,
@@ -48,4 +14,5 @@ const store = configureStore({
 sagaMiddleware.run(rootSaga);
 
 const persistor = persistStore(store);
+
 export { store, persistor };
--- crm/src/store/persistConfig.ts
+++ crm/src/store/persistConfig.ts
@@ -4,17 +4,21 @@ import { combineReducers } from '@reduxjs/toolkit';
 import authReducer from './reducers/authReducer';
 import userReducer from './reducers/userReducer';
 import businessReducer from './reducers/businessReducer';
+import leadPageReducer from './reducers/leadPageReducer';
+import listsPageReducer from './reducers/listsPageReducer';
 
 const persistConfig = {
   key: 'root',
   storage,
-  whitelist: ['auth'], // Add the names of the reducers you want to persist here
+  whitelist: ['auth', 'users'], // Add the names of the reducers you want to persist here
 };
 
 const rootReducer = combineReducers({
   auth: authReducer,
   users: userReducer,
   businesses: businessReducer,
+  lead: leadPageReducer,
+  lists: listsPageReducer,
 });
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
