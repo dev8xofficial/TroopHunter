43a4de72af9f104795bb204d3e057f561484372d
helloabdul <contact@helloabdul.com>
2023-07-27 16:49:26 +0500

    feature/crm - Added animation in Accordion collapsing.

--- crm/src/components/Surfaces/Accordion/Accordion.tsx
+++ crm/src/components/Surfaces/Accordion/Accordion.tsx
@@ -1,4 +1,4 @@
-import { Disclosure } from '@headlessui/react';
+import { Disclosure, Transition } from '@headlessui/react';
 import { Switch } from '@headlessui/react';
 import { PlusIcon, MinusIcon } from '@heroicons/react/20/solid';
 import { IAccordionProps } from './Accordion.interfaces';
@@ -100,25 +100,27 @@ const Accordion: React.FC<IAccordionProps> = ({ label, name, value, handleChange
     <Disclosure as="div">
       {({ open }) => (
         <>
-          <Disclosure.Button className="flex w-full justify-between rounded-lg px-4 py-6 text-left hover:bg-gray-50 focus:outline-none sm:px-6">
+          <Disclosure.Button className={`flex w-full justify-between px-4 py-6 text-left ${open && 'bg-white'} hover:bg-white focus:outline-none sm:px-6`}>
             <span>{label}</span>
             {open ? <MinusIcon className="h-5 w-5 text-gray-400" aria-hidden="true" /> : <PlusIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />}
           </Disclosure.Button>
-          <Disclosure.Panel className="space-y-4 border-t border-dashed p-4 sm:px-6">
-            <div>
-              <span className="inline-flex items-center gap-x-0.5 rounded-md bg-indigo-50 px-2 py-1 text-xs font-medium text-indigo-700 ring-1 ring-inset ring-indigo-700/10">
-                Badge
-                <button type="button" className="group relative -mr-1 h-3.5 w-3.5 rounded-sm hover:bg-indigo-600/20">
-                  <span className="sr-only">Remove</span>
-                  <svg viewBox="0 0 14 14" className="h-3.5 w-3.5 stroke-indigo-700/50 group-hover:stroke-indigo-700/75">
-                    <path d="M4 4l6 6m0-6l-6 6" />
-                  </svg>
-                  <span className="absolute -inset-1" />
-                </button>
-              </span>
-            </div>
-            <div>{getField(name)}</div>
-          </Disclosure.Panel>
+          <Transition enter="transition duration-200 ease-out" enterFrom="transform scale-95 opacity-0" enterTo="transform scale-100 opacity-100" leave="transition duration-75 ease-out" leaveFrom="transform scale-100 opacity-100" leaveTo="transform scale-95 opacity-0">
+            <Disclosure.Panel className="space-y-4 border-t border-dashed bg-white p-4 sm:px-6">
+              <div>
+                <span className="inline-flex items-center gap-x-0.5 rounded-md bg-indigo-50 px-2 py-1 text-xs font-medium text-indigo-700 ring-1 ring-inset ring-indigo-700/10">
+                  Badge
+                  <button type="button" className="group relative -mr-1 h-3.5 w-3.5 rounded-sm hover:bg-indigo-600/20">
+                    <span className="sr-only">Remove</span>
+                    <svg viewBox="0 0 14 14" className="h-3.5 w-3.5 stroke-indigo-700/50 group-hover:stroke-indigo-700/75">
+                      <path d="M4 4l6 6m0-6l-6 6" />
+                    </svg>
+                    <span className="absolute -inset-1" />
+                  </button>
+                </span>
+              </div>
+              <div>{getField(name)}</div>
+            </Disclosure.Panel>
+          </Transition>
         </>
       )}
     </Disclosure>
