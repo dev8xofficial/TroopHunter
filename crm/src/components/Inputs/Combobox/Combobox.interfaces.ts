81d61b3026588de5aab19fe5a1a2b8a1473ade94
helloabdul <contact@helloabdul.com>
2023-07-31 19:33:18 +0500

    feature/crm - Added Combobox component.

---
+++ crm/src/components/Inputs/Combobox/Combobox.interfaces.ts
@@ -0,0 +1,15 @@
+import { ChangeEvent } from 'react';
+
+export interface ILocationComboboxProps {
+  label?: string;
+  type: 'country' | 'state' | 'city'; // New prop to specify the type of location
+  value: string;
+  onChange: (event: ChangeEvent<HTMLInputElement>) => void;
+}
+
+export interface ILocationComboboxOption {
+  id: string;
+  name: string;
+  value: string;
+  code: string;
+}
