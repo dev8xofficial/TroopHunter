f68506bc1ebe0ab98af13d720c07976052c42607
helloabdul <contact@helloabdul.com>
2023-08-04 10:40:05 +0500

    feature/backend - Added custom messages in Business.schema.ts.

--- backend/src/models/Business/Business.messages.ts
+++ backend/src/models/Business/Business.messages.ts
@@ -49,6 +49,74 @@ const messages: Record<string, Message> = {
   },
 
   // Invalid fields messages
+  INVALID_BUSINESS_ID: {
+    code: 400,
+    message: 'Invalid business ID. The business ID provided is not in a valid format. Please provide a valid UUID for the business ID.',
+  },
+  INVALID_BUSINESS_DOMAIN: {
+    code: 400,
+    message: 'Invalid business domain. The business domain must be a non-null string.',
+  },
+  INVALID_CATEGORY_ID: {
+    code: 400,
+    message: 'Invalid category ID. The category ID provided is not in a valid format. Please provide a valid UUID for the category ID.',
+  },
+  INVALID_CITY_ID: {
+    code: 400,
+    message: 'Invalid city ID. The city ID provided is not in a valid format. Please provide a valid UUID for the city ID.',
+  },
+  INVALID_STATE_ID: {
+    code: 400,
+    message: 'Invalid state ID. The state ID provided is not in a valid format. Please provide a valid UUID for the state ID.',
+  },
+  INVALID_COUNTRY_ID: {
+    code: 400,
+    message: 'Invalid country ID. The country ID provided is not in a valid format. Please provide a valid UUID for the country ID.',
+  },
+  INVALID_PHONE_ID: {
+    code: 400,
+    message: 'Invalid phone ID. The phone ID provided is not in a valid format. Please provide a valid UUID for the phone ID.',
+  },
+  INVALID_BUSINESS_EMAIL: {
+    code: 400,
+    message: 'Invalid email. Please provide a valid email address.',
+  },
+  INVALID_BUSINESS_WEBSITE: {
+    code: 400,
+    message: 'Invalid website. Please provide a valid website URL.',
+  },
+  INVALID_BUSINESS_RATING_ID: {
+    code: 400,
+    message: 'Invalid rating ID. The rating ID provided is not in a valid format. Please provide a valid UUID for the rating ID.',
+  },
+  INVALID_BUSINESS_REVIEWS: {
+    code: 400,
+    message: 'Invalid reviews. The reviews must be a valid number.',
+  },
+  INVALID_TIMEZONE_ID: {
+    code: 400,
+    message: 'Invalid timezone ID. The timezone ID must be a string value.',
+  },
+  INVALID_SOURCE_ID: {
+    code: 400,
+    message: 'Invalid source ID. The source ID must be a string value.',
+  },
+  INVALID_SOCIAL_MEDIA_ID: {
+    code: 400,
+    message: 'Invalid social media ID. The social media ID must be a string value.',
+  },
+  INVALID_SPONSORED_AD: {
+    code: 400,
+    message: 'Invalid sponsored ad. The sponsored ad must be a boolean value.',
+  },
+  INVALID_OPENING_HOUR_ID: {
+    code: 400,
+    message: 'Invalid opening hour ID. The opening hour ID must be a string value.',
+  },
+  INVALID_CLOSING_HOUR_ID: {
+    code: 400,
+    message: 'Invalid closing hour ID. The closing hour ID must be a string value.',
+  },
 
   // Duplicate messages
 
--- backend/src/models/Business/Business.schema.ts
+++ backend/src/models/Business/Business.schema.ts
@@ -5,39 +5,16 @@ import { getBusinessMessage } from './Business.messages';
 
 export const BusinessSchema = Joi.object<BusinessAttributes>({
   id: Joi.string(),
-  name: Joi.string()
-    .required()
-    .messages({
-      'string.empty': getBusinessMessage('MISSING_NAME').message,
-      'any.required': getBusinessMessage('MISSING_NAME').message,
-    }),
+  name: Joi.string().required(),
   businessDomain: Joi.string(),
   categoryId: Joi.string(),
-  address: Joi.string()
-    .required()
-    .messages({
-      'string.empty': getBusinessMessage('MISSING_ADDRESS').message,
-      'any.required': getBusinessMessage('MISSING_ADDRESS').message,
-    }),
+  address: Joi.string().required(),
   cityId: Joi.string(),
   stateId: Joi.string(),
   countryId: Joi.string(),
-  longitude: Joi.number()
-    .required()
-    .messages({
-      'number.empty': getBusinessMessage('MISSING_LONGITUDE').message,
-      'any.required': getBusinessMessage('MISSING_LONGITUDE').message,
-    }),
-  latitude: Joi.number()
-    .required()
-    .messages({
-      'number.empty': getBusinessMessage('MISSING_LATITUDE').message,
-      'any.required': getBusinessMessage('MISSING_LATITUDE').message,
-    }),
-  geoPoint: GeoPointSchema.required().messages({
-    'object.empty': getBusinessMessage('MISSING_SOURCE').message,
-    'any.required': getBusinessMessage('MISSING_SOURCE').message,
-  }),
+  longitude: Joi.number().required(),
+  latitude: Joi.number().required(),
+  geoPoint: GeoPointSchema.required(),
   postalCodeId: Joi.string(),
   phoneId: Joi.string(),
   email: Joi.string(),
@@ -51,3 +28,216 @@ export const BusinessSchema = Joi.object<BusinessAttributes>({
   openingHourId: Joi.string(),
   closingHourId: Joi.string(),
 });
+
+export const createErrorResponse = (error: Joi.ValidationError) => {
+  const errorResponse: any = {};
+
+  error.details.forEach((errorDetail) => {
+    switch (errorDetail.context?.key) {
+      case 'id':
+        switch (errorDetail.type) {
+          case 'string.base':
+            errorResponse.error = getBusinessMessage('INVALID_BUSINESS_ID').message;
+            errorResponse.status = getBusinessMessage('INVALID_BUSINESS_ID').code;
+            break;
+        }
+        break;
+      case 'name':
+        switch (errorDetail.type) {
+          case 'string.empty':
+            errorResponse.error = getBusinessMessage('MISSING_NAME').message;
+            errorResponse.status = getBusinessMessage('MISSING_NAME').code;
+            break;
+          case 'any.required':
+            errorResponse.error = getBusinessMessage('MISSING_NAME').message;
+            errorResponse.status = getBusinessMessage('MISSING_NAME').code;
+            break;
+        }
+        break;
+      case 'businessDomain':
+        switch (errorDetail.type) {
+          case 'string.base':
+            errorResponse.error = getBusinessMessage('INVALID_BUSINESS_DOMAIN').message;
+            errorResponse.status = getBusinessMessage('INVALID_BUSINESS_DOMAIN').code;
+            break;
+        }
+        break;
+      case 'categoryId':
+        switch (errorDetail.type) {
+          case 'string.base':
+            errorResponse.error = getBusinessMessage('INVALID_CATEGORY_ID').message;
+            errorResponse.status = getBusinessMessage('INVALID_CATEGORY_ID').code;
+            break;
+        }
+        break;
+      case 'address':
+        switch (errorDetail.type) {
+          case 'string.empty':
+            errorResponse.error = getBusinessMessage('MISSING_ADDRESS').message;
+            errorResponse.status = getBusinessMessage('MISSING_ADDRESS').code;
+            break;
+          case 'any.required':
+            errorResponse.error = getBusinessMessage('MISSING_ADDRESS').message;
+            errorResponse.status = getBusinessMessage('MISSING_ADDRESS').code;
+            break;
+        }
+        break;
+      case 'cityId':
+        switch (errorDetail.type) {
+          case 'string.base':
+            errorResponse.error = getBusinessMessage('INVALID_CITY_ID').message;
+            errorResponse.status = getBusinessMessage('INVALID_CITY_ID').code;
+            break;
+        }
+        break;
+      case 'stateId':
+        switch (errorDetail.type) {
+          case 'string.base':
+            errorResponse.error = getBusinessMessage('INVALID_STATE_ID').message;
+            errorResponse.status = getBusinessMessage('INVALID_STATE_ID').code;
+            break;
+        }
+        break;
+      case 'countryId':
+        switch (errorDetail.type) {
+          case 'string.base':
+            errorResponse.error = getBusinessMessage('INVALID_COUNTRY_ID').message;
+            errorResponse.status = getBusinessMessage('INVALID_COUNTRY_ID').code;
+            break;
+        }
+        break;
+      case 'longitude':
+        switch (errorDetail.type) {
+          case 'number.empty':
+            errorResponse.error = getBusinessMessage('MISSING_LONGITUDE').message;
+            errorResponse.status = getBusinessMessage('MISSING_LONGITUDE').code;
+            break;
+          case 'any.required':
+            errorResponse.error = getBusinessMessage('MISSING_LONGITUDE').message;
+            errorResponse.status = getBusinessMessage('MISSING_LONGITUDE').code;
+            break;
+        }
+        break;
+      case 'latitude':
+        switch (errorDetail.type) {
+          case 'number.empty':
+            errorResponse.error = getBusinessMessage('MISSING_LATITUDE').message;
+            errorResponse.status = getBusinessMessage('MISSING_LATITUDE').code;
+            break;
+          case 'any.required':
+            errorResponse.error = getBusinessMessage('MISSING_LATITUDE').message;
+            errorResponse.status = getBusinessMessage('MISSING_LATITUDE').code;
+            break;
+        }
+        break;
+      case 'geoPoint':
+        switch (errorDetail.type) {
+          case 'object.empty':
+          case 'any.required':
+            errorResponse.error = getBusinessMessage('MISSING_SOURCE').message;
+            errorResponse.status = getBusinessMessage('MISSING_SOURCE').code;
+            break;
+        }
+        break;
+      case 'postalCodeId':
+        switch (errorDetail.type) {
+          case 'string.base':
+            errorResponse.error = getBusinessMessage('INVALID_POSTAL_CODE_ID').message;
+            errorResponse.status = getBusinessMessage('INVALID_POSTAL_CODE_ID').code;
+            break;
+        }
+        break;
+      case 'phoneId':
+        switch (errorDetail.type) {
+          case 'string.base':
+            errorResponse.error = getBusinessMessage('INVALID_PHONE_ID').message;
+            errorResponse.status = getBusinessMessage('INVALID_PHONE_ID').code;
+            break;
+        }
+        break;
+      case 'email':
+        switch (errorDetail.type) {
+          case 'string.base':
+            errorResponse.error = getBusinessMessage('INVALID_BUSINESS_EMAIL').message;
+            errorResponse.status = getBusinessMessage('INVALID_BUSINESS_EMAIL').code;
+            break;
+        }
+        break;
+      case 'website':
+        switch (errorDetail.type) {
+          case 'string.base':
+            errorResponse.error = getBusinessMessage('INVALID_BUSINESS_WEBSITE').message;
+            errorResponse.status = getBusinessMessage('INVALID_BUSINESS_WEBSITE').code;
+            break;
+        }
+        break;
+      case 'ratingId':
+        switch (errorDetail.type) {
+          case 'string.base':
+            errorResponse.error = getBusinessMessage('INVALID_BUSINESS_RATING_ID').message;
+            errorResponse.status = getBusinessMessage('INVALID_BUSINESS_RATING_ID').code;
+            break;
+        }
+        break;
+      case 'reviews':
+        switch (errorDetail.type) {
+          case 'number.base':
+            errorResponse.error = getBusinessMessage('INVALID_BUSINESS_REVIEWS').message;
+            errorResponse.status = getBusinessMessage('INVALID_BUSINESS_REVIEWS').code;
+            break;
+        }
+        break;
+      case 'timezoneId':
+        switch (errorDetail.type) {
+          case 'string.base':
+            errorResponse.error = getBusinessMessage('INVALID_TIMEZONE_ID').message;
+            errorResponse.status = getBusinessMessage('INVALID_TIMEZONE_ID').code;
+            break;
+        }
+        break;
+      case 'sourceId':
+        switch (errorDetail.type) {
+          case 'string.base':
+            errorResponse.error = getBusinessMessage('INVALID_SOURCE_ID').message;
+            errorResponse.status = getBusinessMessage('INVALID_SOURCE_ID').code;
+            break;
+        }
+        break;
+      case 'socialMediaId':
+        switch (errorDetail.type) {
+          case 'string.base':
+            errorResponse.error = getBusinessMessage('INVALID_SOCIAL_MEDIA_ID').message;
+            errorResponse.status = getBusinessMessage('INVALID_SOCIAL_MEDIA_ID').code;
+            break;
+        }
+        break;
+      case 'sponsoredAd':
+        switch (errorDetail.type) {
+          case 'boolean.base':
+            errorResponse.error = getBusinessMessage('INVALID_SPONSORED_AD').message;
+            errorResponse.status = getBusinessMessage('INVALID_SPONSORED_AD').code;
+            break;
+        }
+        break;
+      case 'openingHourId':
+        switch (errorDetail.type) {
+          case 'string.base':
+            errorResponse.error = getBusinessMessage('INVALID_OPENING_HOUR_ID').message;
+            errorResponse.status = getBusinessMessage('INVALID_OPENING_HOUR_ID').code;
+            break;
+        }
+        break;
+      case 'closingHourId':
+        switch (errorDetail.type) {
+          case 'string.base':
+            errorResponse.error = getBusinessMessage('INVALID_CLOSING_HOUR_ID').message;
+            errorResponse.status = getBusinessMessage('INVALID_CLOSING_HOUR_ID').code;
+            break;
+        }
+        break;
+      default:
+        break;
+    }
+  });
+  return errorResponse;
+};
