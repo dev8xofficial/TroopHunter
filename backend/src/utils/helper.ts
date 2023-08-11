4f620bc8217dcdc6452050bd4ea3501c419e5bd6
helloabdul <contact@helloabdul.com>
2023-08-11 19:32:57 +0500

    feature/backend - Working on query fields for all endpoints.

--- backend/src/utils/helper.ts
+++ backend/src/utils/helper.ts
@@ -1,3 +1,39 @@
+import { Request, Response, NextFunction } from 'express';
+import qs from 'qs';
+
+export const parseFieldsAndApplySelection = (req: Request, res: Response, next: NextFunction) => {
+  const { fields }: any = req.query;
+
+  if (fields) {
+    try {
+      const parsedFields = qs.parse(fields);
+
+      const applyFieldSelection = (data: any, fields: any): any => {
+        if (Array.isArray(data)) {
+          return data.map((item) => applyFieldSelection(item, fields));
+        } else if (typeof data === 'object') {
+          const selectedData: any = {};
+          for (const field in fields) {
+            if (fields[field]) {
+              selectedData[field] = applyFieldSelection(data[field], fields[field]);
+            }
+          }
+          return selectedData;
+        } else {
+          return data;
+        }
+      };
+
+      // Apply field selection logic directly to the response data
+      res.locals.data = applyFieldSelection(res.locals.data, parsedFields);
+    } catch (error) {
+      return res.status(400).json({ error: 'Invalid fields query' });
+    }
+  }
+
+  next();
+};
+
 export const isValidJSON = (jsonString: string) => {
   try {
     JSON.parse(jsonString);
