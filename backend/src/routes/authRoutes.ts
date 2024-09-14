234f2389e67fe7afc1720af5a2c8fb01ecb3cebc
helloabdul <contact@helloabdul.com>
2024-09-14 05:52:14 +0500

    feature/backend - Remove auth code from backend.

--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -1,11 +1,10 @@
 import express from 'express';
-import { login, refreshToken, register, verifyUser, signOut } from '../controllers/AuthController';
-import { VerifyUserValidationMiddleware, RefreshTokenValidationMiddleware, UserCreateRequestValidationMiddleware, UserFetchByIdRequestValidationMiddleware } from 'validator/validators';
+import { login, refreshToken, register, signOut } from '../controllers/AuthController';
+import { RefreshTokenValidationMiddleware, UserCreateRequestValidationMiddleware, UserFetchByIdRequestValidationMiddleware } from 'validator/validators';
 import { LoginRequestValidationMiddleware } from 'validator/validators';
 
 const router = express.Router();
 
-router.get('/verify/:id/:token', VerifyUserValidationMiddleware, verifyUser);
 router.post('/refresh-token', RefreshTokenValidationMiddleware, refreshToken);
 router.post('/signin', LoginRequestValidationMiddleware, login);
 router.post('/signup', UserCreateRequestValidationMiddleware, register);
