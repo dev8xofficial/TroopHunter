76988b4a372e57555a7539f591b51817ab0c305a
helloabdul <contact@helloabdul.com>
2024-10-01 09:30:41 +0500

    feature/crm - Improving pwa icons.

--- crm/src/App.tsx
+++ crm/src/App.tsx
@@ -1,30 +1,37 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React from 'react';
+// import React, { useEffect, useRef, useState } from 'react';
 
 import AppRouter from './routes/AppRouter';
 
 const App: React.FC = () => {
-  const [loading, setLoading] = useState<boolean>(true);
-  const preloaderRef = useRef<HTMLDivElement>(null);
+  // const [loading, setLoading] = useState<boolean>(true);
+  // const preloaderRef = useRef<HTMLDivElement>(null);
 
-  useEffect(() => {
-    const preloader = preloaderRef.current;
-    if (preloader != null) {
-      setTimeout(() => {
-        preloader.classList.toggle('hidden');
-        setLoading(false);
-      }, 2000);
-    } else {
-      setLoading(false);
-    }
-  }, [loading]);
+  // useEffect(() => {
+  //   const preloader = preloaderRef.current;
+  //   if (preloader != null) {
+  //     setTimeout(() => {
+  //       preloader.classList.toggle('hidden');
+  //       setLoading(false);
+  //     }, 2000);
+  //   } else {
+  //     setLoading(false);
+  //   }
+  // }, [loading]);
 
-  return loading ? (
-    <p className="text-danger text-center">Failed to load app</p>
-  ) : (
+  // return loading ? (
+  //   <p className="text-danger text-center">Failed to load app</p>
+  // ) : (
+  //   <>
+  //     <div className="fixed left-0 top-0 z-50 flex h-screen w-screen touch-none flex-col items-center justify-center bg-white" ref={preloaderRef}>
+  //       <img className="mx-auto h-8 animate-pulse lg:h-10" src={`${import.meta.env.VITE_TROOPHUNTER_PUBLIC_URL}/logo/logo.svg`} alt="TroopHunter" />
+  //     </div>
+  //     <AppRouter />
+  //   </>
+  // );
+
+  return (
     <>
-      <div className="fixed left-0 top-0 z-50 flex h-screen w-screen touch-none flex-col items-center justify-center bg-white" ref={preloaderRef}>
-        <img className="mx-auto h-8 animate-pulse lg:h-10" src={`${import.meta.env.VITE_TROOPHUNTER_PUBLIC_URL}/logo/logo.svg`} alt="TroopHunter" />
-      </div>
       <AppRouter />
     </>
   );
