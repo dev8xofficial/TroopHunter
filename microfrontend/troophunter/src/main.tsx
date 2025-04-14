7ae2a4c062ad38725b8bf51854a23cca9dfad183
helloabdul <contact@helloabdul.com>
2025-04-14 07:28:20 +0500

    Trying to figure out why app.troophunter.com is not working after deploying it on vercel.

--- microfrontend/troophunter/src/main.tsx
+++ microfrontend/troophunter/src/main.tsx
@@ -1,43 +1,43 @@
-// import React from 'react';
-
-// import ReactDOM from 'react-dom/client';
-// import { HelmetProvider } from 'react-helmet-async';
-// import { Provider } from 'react-redux';
-// import { BrowserRouter as Router } from 'react-router-dom';
-// import { ToastContainer, Flip } from 'react-toastify';
-// import { PersistGate } from 'redux-persist/integration/react';
-
-// import App from './App';
-// import { store, persistor } from './store';
-
-// // Import css files
-// import 'react-toastify/dist/ReactToastify.css';
-// import './index.css';
-
-// ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-//   <React.StrictMode>
-//     <Provider store={store}>
-//       <PersistGate loading={null} persistor={persistor}>
-//         <ToastContainer position="top-right" autoClose={5000} hideProgressBar={false} newestOnTop={true} closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover transition={Flip} />
-
-//         <HelmetProvider>
-//           <Router>
-//             <App />
-//           </Router>
-//         </HelmetProvider>
-//       </PersistGate>
-//     </Provider>
-//   </React.StrictMode>
-// );
-
 import React from 'react';
 
 import ReactDOM from 'react-dom/client';
+import { HelmetProvider } from 'react-helmet-async';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { ToastContainer, Flip } from 'react-toastify';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import App from './App';
+import { store, persistor } from './store';
+
+// Import css files
+import 'react-toastify/dist/ReactToastify.css';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <ToastContainer position="top-right" autoClose={5000} hideProgressBar={false} newestOnTop={true} closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover transition={Flip} />
+
+        <HelmetProvider>
+          <Router>
+            <App />
+          </Router>
+        </HelmetProvider>
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
 );
+
+// import React from 'react';
+
+// import ReactDOM from 'react-dom/client';
+
+// import App from './App';
+
+// ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
