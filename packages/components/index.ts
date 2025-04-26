2aa33c7c7e01a1f8f92cde8a17c53785347e45db
ahmadsohaib <ahmadsohaib@dev8x.com>
2025-04-26 06:05:36 +0500

    Add microfrontend components in packages folder.

---
+++ packages/components/index.ts
@@ -0,0 +1,5 @@
+export * from './src/Feedback/VideoDialog';
+export * from './src/Surfaces/Header';
+export * from './src/Surfaces/Hero';
+export * from './src/Surfaces/Problems';
+export * from './src/Surfaces/Steps';
