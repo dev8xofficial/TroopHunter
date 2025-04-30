75cbbe4fa63953caa1086c56ac055cbae7d464ba
helloabdul <contact@helloabdul.com>
2025-05-01 04:15:42 +0500

    Created all expertise pages.

---
+++ microfrontend/dev8X/pages/expertise/community/index.tsx
@@ -0,0 +1,209 @@
+import React, { useEffect } from 'react';
+
+// import { gsap } from 'gsap';
+// import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
+// import Scrollbar from 'smooth-scrollbar';
+import Head from 'next/head';
+import { FooterRevealPageWrap, ContentAsideImage, Footer, Header, Hero, IconCards, ModularBlocks } from '@repo/components';
+import { PageData } from './index.d';
+
+import styles from './index.module.css';
+
+// type ScrollTriggerCallback = () => void;
+
+// const createBackgroundScrollTrigger = (sectionId: string, startTrigger: string, endTrigger: string, onEnter?: ScrollTriggerCallback, onLeave?: ScrollTriggerCallback, onLeaveBack?: ScrollTriggerCallback, onEnterBack?: ScrollTriggerCallback): void => {
+//   ScrollTrigger.create({
+//     trigger: sectionId,
+//     scroller: '#smooth-scrollbar',
+//     start: startTrigger,
+//     end: endTrigger,
+//     onEnter:
+//       onEnter != null
+//         ? () => {
+//             onEnter();
+//           }
+//         : undefined,
+//     onLeave:
+//       onLeave != null
+//         ? () => {
+//             onLeave();
+//           }
+//         : undefined,
+//     onLeaveBack:
+//       onLeaveBack != null
+//         ? () => {
+//             onLeaveBack();
+//           }
+//         : undefined,
+//     onEnterBack:
+//       onEnterBack != null
+//         ? () => {
+//             onEnterBack();
+//           }
+//         : undefined
+//   });
+// };
+
+const Contact: React.FC = (): JSX.Element => {
+  // useEffect(() => {
+  //   if (typeof window !== 'undefined') {
+  //     gsap.registerPlugin(ScrollTrigger);
+
+  //     const scrollbar = Scrollbar.init(document.querySelector('#smooth-scrollbar') as HTMLElement, {
+  //       damping: 0.05
+  //     });
+
+  //     ScrollTrigger.scrollerProxy('#smooth-scrollbar', {
+  //       scrollTop(value) {
+  //         if (arguments.length) {
+  //           scrollbar.scrollTop = value;
+  //         }
+  //         return scrollbar.scrollTop;
+  //       },
+  //       getBoundingClientRect() {
+  //         return { top: 0, left: 0, width: window.innerWidth, height: window.innerHeight };
+  //       }
+  //     });
+
+  //     scrollbar.addListener(ScrollTrigger.update);
+
+  //     const sections = document.querySelectorAll('section');
+  //     sections.forEach((section) => {
+  //       gsap.fromTo(
+  //         section,
+  //         { y: 50 },
+  //         {
+  //           y: 0,
+  //           scrollTrigger: {
+  //             trigger: section,
+  //             scroller: '#smooth-scrollbar',
+  //             start: 'top 80%',
+  //             end: 'bottom 20%',
+  //             scrub: 2
+  //           },
+  //           ease: 'power2.in',
+  //           duration: 2
+  //         }
+  //       );
+  //     });
+
+  //     const mainTag = document.querySelector('main') as HTMLElement;
+
+  //     // Code for FeatureVideo
+  //     // const triggerElment = document.getElementById('hero-section');
+  //     // const featureVideoWrapperElement = document.getElementById('feature-video-wrapper');
+  //     // let featureVideoElement = document.getElementById('feature-video');
+
+  //     // scrollbar.addListener(function (status) {
+  //     //   var offset = status.offset;
+
+  //     //   console.log('offset.y: ', offset.y);
+  //     //   featureVideoElement.style.top = `calc(${offset.y + 'px' + ' - 5vh'})`;
+  //     // });
+
+  //     // ScrollTrigger.create({
+  //     //   trigger: featureVideoElement,
+  //     //   scroller: '#smooth-scrollbar',
+  //     //   start: 'top 50%',
+  //     //   end: 'bottom bottom',
+  //     //   scrub: true, // Enable smooth scrubbing (this will automatically reverse the animation on scroll up)
+  //     //   onUpdate: (self) => {
+  //     //     featureVideoElement.style.setProperty('--progress', self.progress.toString());
+  //     //   }
+  //     // });
+
+  //     createBackgroundScrollTrigger(
+  //       '#footer-section',
+  //       'top 10%',
+  //       'top 10%',
+  //       () => {
+  //         if (mainTag != null) {
+  //           mainTag.style.backgroundColor = '#dcf5f2';
+  //         }
+  //       },
+  //       () => {
+  //         if (mainTag != null) {
+  //           mainTag.style.backgroundColor = '#dcf5f2';
+  //         }
+  //       },
+  //       () => {
+  //         if (mainTag != null) {
+  //           mainTag.style.backgroundColor = '';
+  //         }
+  //       },
+  //       () => {
+  //         if (mainTag != null) {
+  //           mainTag.style.backgroundColor = '#dcf5f2';
+  //         }
+  //       }
+  //     );
+
+  //     createBackgroundScrollTrigger(
+  //       '#footer-section',
+  //       'top top',
+  //       'bottom top',
+  //       () => {
+  //         if (mainTag != null) {
+  //           mainTag.style.backgroundColor = '';
+  //         }
+  //       },
+  //       () => {},
+  //       () => {
+  //         if (mainTag != null) {
+  //           mainTag.style.backgroundColor = '#f3f3e9';
+  //         }
+  //       }
+  //     );
+
+  //     return () => {
+  //       ScrollTrigger.getAll().forEach((trigger) => trigger.kill());
+  //       scrollbar.destroy();
+  //     };
+  //   }
+  // }, []);
+
+  return (
+    <>
+      <Head>
+        <title>Dev8X - Solutions Made Simple!</title>
+        <meta name="description" content="TroopHunter simplifies finding and connecting with businesses around the world."></meta>
+        <link rel="canonical" href={`${process.env.VITE_TROOPHUNTER_PUBLIC_URL}`} />
+
+        {/* Open Graph Tags */}
+        <meta property="og:title" content="TroopHunter - Find your next client!"></meta>
+        <meta property="og:description" content="TroopHunter simplifies finding and connecting with businesses around the world."></meta>
+        <meta property="og:url" content={`${process.env.VITE_TROOPHUNTER_PUBLIC_URL}`}></meta>
+        <meta property="og:locale" content="en_US"></meta>
+        <meta property="og:image" content={`${process.env.VITE_TROOPHUNTER_PUBLIC_URL}/logo/logo-social.png`}></meta>
+        <meta property="og:image:secure_url" content={`${process.env.VITE_TROOPHUNTER_PUBLIC_URL}/logo/logo-social.png`}></meta>
+        <meta property="og:type" content="website"></meta>
+        <meta property="og:site_name" content="TroopHunter"></meta>
+
+        {/* Twitter Card Tags */}
+        <meta name="twitter:card" content="summary_large_image"></meta>
+        <meta name="twitter:title" content="TroopHunter - Find your next client!"></meta>
+        <meta name="twitter:description" content="TroopHunter simplifies finding and connecting with businesses around the world."></meta>
+        <meta name="twitter:image" content={`${process.env.VITE_TROOPHUNTER_PUBLIC_URL}/logo/logo-social.png`}></meta>
+        <meta name="twitter:site" content="@TroopHunter"></meta>
+      </Head>
+      <FooterRevealPageWrap variant="frame">
+        <Header />
+        <FooterRevealPageWrap variant="page">
+          {/* Main container with smooth-scrollbar */}
+          <main className={`${styles['expertise-single']} container-full`}>
+            <Hero tagText="Community" heading="Interactive, purposeful experiences" image="https://a-us.storyblok.com/f/1017006/3810x2036/6fe5a4bbe5/sussex-3810-x-2039.jpg" />
+            <div>
+              <ModularBlocks>
+                <IconCards title="A force for good" paragraph="Blending award-winning UX, delightful design, and meaningful messages, our world-class digital products continue to serve the community." IconCardsItems={PageData.IconCardsItems} />
+                <ContentAsideImage ContentAsideImageItems={PageData.ContentAsideImageItems} />
+              </ModularBlocks>
+            </div>
+          </main>
+        </FooterRevealPageWrap>
+        <Footer footerMainContent={PageData.footerMainContent} footerForm={PageData.footerForm} footerSocialLinks={PageData.footerSocialLinks} />
+      </FooterRevealPageWrap>
+    </>
+  );
+};
+
+export default Contact;
---
+++ microfrontend/dev8X/pages/expertise/ecommerce/index.d.tsx
@@ -0,0 +1,114 @@
+import { CSSProperties } from 'react';
+
+export const PageData = {
+  IconCardsItems: [
+    {
+      title: 'Seamless Online Retail',
+      description: 'Simplified administration and automation, high-performing customer experiences and ongoing optimisation for pureplay and omni-channel retailers.',
+      icon: (
+        <svg width="28" height="28" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg" style={{ '--width': 28, '--height': 28 } as CSSProperties}>
+          <path d="M9.46167 10.1617L9.89334 6.60334C10.1267 4.67834 11.9467 3.30166 13.965 3.52333C15.9833 3.74499 17.43 5.48333 17.1967 7.40833L16.835 10.43" stroke="var(--theme-primary)" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M19.7399 8.75L18.7132 24.5L5.03991 23.0883C4.40991 23.0183 3.97824 22.435 4.11824 21.8167L6.98824 8.75H19.7399V8.75Z" stroke="var(--theme-secondary)" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>{' '}
+          <path d="M23.45 21.2917L18.7017 24.5L19.7283 8.75L22.9017 9.765L23.905 20.3233C23.94 20.7083 23.765 21.07 23.45 21.2917Z" stroke="var(--theme-secondary)" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>{' '}
+        </svg>
+      )
+    },
+    {
+      title: 'Butter-smooth Checkouts',
+      description: 'We apply our award-winning UX and design approach with analytical data and best-practice UX principles to convert spend and minimise abandonment.',
+      icon: (
+        <svg width="25" height="26" viewBox="0 0 25 26" fill="none" xmlns="http://www.w3.org/2000/svg" style={{ '--width': 25, '--height': 26 } as CSSProperties}>
+          <path d="M4.5 8L23 7.5L21.3628 14.4581C21.1502 15.3615 20.3441 16 19.416 16H8.47038C7.59561 16 6.82234 15.4315 6.56142 14.5965L4.5 8Z" fill="var(--theme-primary)"></path>{' '}
+          <path
+            d="M23.9984 6.5975C23.9143 6.49679 23.809 6.41579 23.6901 6.36021C23.5712 6.30463 23.4416 6.27584 23.3103 6.27586H5.23138L4.68336 3.26569C4.6083 2.85253 4.39062 2.47882 4.06826 2.20971C3.7459 1.9406 3.33932 1.79316 2.9194 1.7931H0.896552C0.658771 1.7931 0.43073 1.88756 0.262594 2.0557C0.0944578 2.22383 0 2.45187 0 2.68965C0 2.92743 0.0944578 3.15548 0.262594 3.32361C0.43073 3.49175 0.658771 3.58621 0.896552 3.58621H2.91379L5.77828 19.3084C5.86265 19.7746 6.06871 20.2103 6.3756 20.5714C5.95203 20.967 5.64631 21.4722 5.49234 22.031C5.33837 22.5897 5.34216 23.1802 5.50329 23.737C5.66443 24.2937 5.97661 24.795 6.40522 25.1851C6.83383 25.5753 7.36212 25.8391 7.93152 25.9473C8.50092 26.0556 9.08917 26.004 9.63104 25.7983C10.1729 25.5926 10.6472 25.2409 11.0014 24.7821C11.3556 24.3233 11.5758 23.7754 11.6376 23.1991C11.6994 22.6228 11.6003 22.0407 11.3515 21.5172H16.4416C16.2411 21.9371 16.1373 22.3967 16.1379 22.8621C16.1379 23.4827 16.322 24.0894 16.6668 24.6054C17.0116 25.1214 17.5016 25.5236 18.075 25.7611C18.6484 25.9986 19.2793 26.0608 19.888 25.9397C20.4967 25.8186 21.0559 25.5198 21.4947 25.0809C21.9336 24.6421 22.2324 24.0829 22.3535 23.4742C22.4746 22.8655 22.4124 22.2346 22.1749 21.6612C21.9374 21.0879 21.5352 20.5978 21.0192 20.253C20.5032 19.9082 19.8965 19.7241 19.2759 19.7241H8.42422C8.21426 19.7241 8.01097 19.6504 7.84979 19.5158C7.68861 19.3813 7.57977 19.1944 7.54224 18.9878L7.18698 17.0345H20.187C20.8169 17.0344 21.4267 16.8132 21.9103 16.4096C22.3938 16.0059 22.7203 15.4453 22.8329 14.8256L24.1957 7.33267C24.2188 7.20316 24.213 7.07017 24.179 6.94311C24.1449 6.81606 24.0832 6.69807 23.9984 6.5975ZM9.86207 22.8621C9.86207 23.128 9.7832 23.3881 9.63542 23.6092C9.48765 23.8304 9.27762 24.0027 9.03188 24.1045C8.78615 24.2063 8.51575 24.2329 8.25488 24.1811C7.99401 24.1292 7.75438 24.0011 7.5663 23.813C7.37823 23.6249 7.25014 23.3853 7.19825 23.1244C7.14636 22.8636 7.173 22.5932 7.27478 22.3474C7.37657 22.1017 7.54894 21.8917 7.7701 21.7439C7.99125 21.5961 8.25126 21.5172 8.51724 21.5172C8.87391 21.5172 9.21597 21.6589 9.46818 21.9111C9.72038 22.1633 9.86207 22.5054 9.86207 22.8621ZM20.6207 22.8621C20.6207 23.128 20.5418 23.3881 20.394 23.6092C20.2463 23.8304 20.0362 24.0027 19.7905 24.1045C19.5448 24.2063 19.2744 24.2329 19.0135 24.1811C18.7526 24.1292 18.513 24.0011 18.3249 23.813C18.1368 23.6249 18.0088 23.3853 17.9569 23.1244C17.905 22.8636 17.9316 22.5932 18.0334 22.3474C18.1352 22.1017 18.3076 21.8917 18.5287 21.7439C18.7499 21.5961 19.0099 21.5172 19.2759 21.5172C19.6325 21.5172 19.9746 21.6589 20.2268 21.9111C20.479 22.1633 20.6207 22.5054 20.6207 22.8621ZM21.069 14.5051C21.0313 14.7122 20.922 14.8995 20.7601 15.0342C20.5982 15.1688 20.3942 15.2421 20.1836 15.2414H6.86086L5.5575 8.06896H22.2356L21.069 14.5051Z"
+            fill="var(--theme-secondary)"
+          ></path>
+        </svg>
+      )
+    },
+    {
+      title: 'Securely Integrated',
+      description: 'We can connect and configure your business systems and payment solutions to ensure data integrity throughout your entire infrastructure.',
+      icon: (
+        <svg width="24" height="26" viewBox="0 0 24 26" fill="none" xmlns="http://www.w3.org/2000/svg" style={{ '--width': 24, '--height': 26 } as CSSProperties}>
+          <path fill-rule="evenodd" clip-rule="evenodd" d="M16.1416 10.6343C16.5321 11.0248 16.5321 11.658 16.1416 12.0485L11.6492 16.5409C11.26 16.93 10.6296 16.9316 10.2385 16.5445L8.73094 15.0521C8.33844 14.6636 8.33523 14.0304 8.72376 13.6379C9.1123 13.2454 9.74546 13.2422 10.138 13.6307L10.9385 14.4231L14.7273 10.6343C15.1179 10.2438 15.751 10.2438 16.1416 10.6343Z" fill="var(--theme-primary)"></path>{' '}
+          <path
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+            d="M10.3098 0.594436C11.1516 -0.19932 12.4609 -0.198144 13.3012 0.597966C13.3298 0.625079 13.3566 0.653609 13.3814 0.68337C14.9696 2.15065 18.4337 5.09121 22.2183 6.70201M11.8829 2.01935C11.8176 1.98574 11.7385 1.99591 11.6816 2.04987L11.6752 2.05594L11.6752 2.05591C10.0958 3.52596 6.35655 6.75643 2.14732 8.54267C2.05869 8.58034 1.99572 8.66946 1.99916 8.78397C2.1078 10.5948 2.55027 14.5342 4.33498 17.162L4.33703 17.165C6.47174 20.333 10.2065 22.9633 11.7165 23.97C11.7532 23.9944 11.7827 24 11.8037 24C11.8247 24 11.8542 23.9944 11.8908 23.97L11.9031 23.9619C13.4109 22.9884 17.0763 20.4043 19.1869 17.284C20.9904 14.6116 21.4655 10.5861 21.5851 8.76838L21.5853 8.76601C21.5909 8.6836 21.543 8.58815 21.436 8.54267L21.4355 8.54248C17.2386 6.75629 13.4995 3.52587 11.9202 2.05591C11.9074 2.044 11.8949 2.03181 11.8829 2.01935ZM10.3098 0.594436C8.76866 2.02862 5.23984 5.05781 1.36556 6.70179L1.36504 6.70201C0.495108 7.07173 -0.0412858 7.94046 0.00139524 8.87944L0.00129582 8.87944L0.0021353 8.89353C0.110388 10.7098 0.557131 15.1579 2.67939 18.284C5.05567 21.8097 9.09411 24.6254 10.6071 25.6341C11.3366 26.1204 12.2626 26.122 12.9934 25.6387C14.5365 24.6418 18.4944 21.8778 20.8439 18.404L20.8445 18.4032C22.9754 15.2458 23.4602 10.7309 23.5807 8.90087C23.6465 7.92821 23.0709 7.0646 22.2187 6.70221"
+            fill="var(--theme-secondary"
+          ></path>
+        </svg>
+      )
+    },
+    {
+      title: 'eCommerce Partners',
+      description: 'We’re in this for the long haul. We’ll work in close collaboration with you to ensure extraordinary outcomes and your ongoing success over time.',
+      icon: (
+        <svg width="27" height="24" viewBox="0 0 27 24" fill="none" xmlns="http://www.w3.org/2000/svg" style={{ '--width': 27, '--height': 24 } as CSSProperties}>
+          <path d="M10.3731 9.4001C12.7894 9.4001 14.7482 7.51968 14.7482 5.20005C14.7482 2.88043 12.7894 1 10.3731 1C7.95683 1 5.99805 2.88043 5.99805 5.20005C5.99805 7.51968 7.95683 9.4001 10.3731 9.4001Z" stroke="var(--theme-secondary)" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M18.8789 1.59998C20.1494 2.33492 20.9992 3.67221 20.9992 5.20002C20.9992 6.72783 20.1494 8.06512 18.8789 8.80006" stroke="var(--theme-primary)" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>{' '}
+          <path d="M11.3501 14.2002H9.40011C9.02861 14.2002 8.84286 14.2002 8.68584 14.2064C4.51427 14.3703 1.17012 17.7145 1.00617 21.886C1 22.0431 1 22.2288 1 22.6003H19.7502C19.7502 22.2288 19.7502 22.0431 19.7441 21.886C19.5801 17.7145 16.236 14.3703 12.0644 14.2064C11.9074 14.2002 11.7216 14.2002 11.3501 14.2002Z" stroke="var(--theme-secondary)" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>{' '}
+          <path d="M25.9996 22.6002V21.8802C25.9996 19.1919 25.9996 17.8478 25.4546 16.821C24.9753 15.9178 24.2104 15.1835 23.2695 14.7233" stroke="var(--theme-primary)" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>{' '}
+        </svg>
+      )
+    }
+  ],
+  ContentAsideImageItems: [
+    {
+      title: 'Tell a story with data',
+      paragraph: 'Transform complex datasets into immersive narratives that inform and inspire action. Our approach combines rich visuals with insightful analysis, creating a seamless and engaging experience. Whether you aim to drive change, highlight trends, or demonstrate impact, curate your data in the best possible way for powerful digital experiences with lasting impact.',
+      image: 'https://a-us.storyblok.com/f/1017006/1864x1314/d3a8753394/golden-pipeline.jpg'
+    },
+    {
+      title: 'Engaging reports and dashboards',
+      paragraph: 'Surface insights from raw data with visually compelling, interactive experiences and consolidated touchpoints. We design and create data-driven interfaces to suit any objective, from immersive reports for public consumption to bespoke internal tools and dashboards that support and inform stakeholders.',
+      image: 'https://a-us.storyblok.com/f/1017006/1864x1314/aec4c6cecc/data.png'
+    },
+    {
+      title: 'Your data, our expertise',
+      paragraph: 'Leverage our expertise and transform your data into unique and compelling experiences that educate, inspire and delight. By blending your data with our experience in information graphics, data visualisation and modern interface design, we help unlock the full potential and create digital experiences with lasting impact.',
+      image: 'https://a-us.storyblok.com/f/1017006/1864x1314/c1a8cc32bd/fluidity.jpg'
+    }
+  ],
+  footerMainContent: {
+    link: '/contact',
+    start: 'Let’s find',
+    end: 'your next client!'
+  },
+  footerForm: {
+    privacy: {
+      year: '2025',
+      text: 'Privacy'
+    },
+    button: {
+      text: 'Submit a brief'
+    }
+  },
+  footerSocialLinks: [
+    {
+      title: (
+        <>
+          <s>Twitter</s> X
+        </>
+      ),
+      icon: (
+        <svg xmlns="http://www.w3.org/2000/svg" width="13" height="14" fill="none" viewBox="0 0 13 14" className="var-w-13 var-h-14">
+          <path fill="currentColor" fillRule="evenodd" d="M7.121.87H5.874v4.123L2.96 2.078l-.882.882 2.92 2.919H.864v1.247h4.133l-2.919 2.919.882.882 2.913-2.913v4.122h1.247V8.004l2.923 2.923.882-.882-2.919-2.919h4.125V5.88H8.009l2.919-2.919-.882-.882-2.925 2.925V.869Z" clipRule="evenodd" />
+        </svg>
+      ),
+      href: 'https://x.com/dev8x'
+    },
+    {
+      title: 'Instagram',
+      icon: (
+        <svg xmlns="http://www.w3.org/2000/svg" width="13" height="14" fill="none" viewBox="0 0 13 14" className="var-w-13 var-h-14">
+          <path fill="currentColor" fillRule="evenodd" d="M7.121.87H5.874v4.123L2.96 2.078l-.882.882 2.92 2.919H.864v1.247h4.133l-2.919 2.919.882.882 2.913-2.913v4.122h1.247V8.004l2.923 2.923.882-.882-2.919-2.919h4.125V5.88H8.009l2.919-2.919-.882-.882-2.925 2.925V.869Z" clipRule="evenodd" />
+        </svg>
+      ),
+      href: 'https://instagram.com/dev8x'
+    },
+    {
+      title: 'LinkedIn',
+      href: 'https://www.linkedin.com/company/dev8x/posts/'
+    }
+  ]
+};
