fe69aee5fac7e12df74e8caaf8c73881dcb9028b
helloabdul <contact@helloabdul.com>
2025-05-15 08:39:09 +0500

    Remove commented anchors from homepage.

--- microfrontend/dev8X/pages/index.tsx
+++ microfrontend/dev8X/pages/index.tsx
@@ -76,30 +76,6 @@ export default function Home() {
             <div className={styles['homepage__section']}>
               <WorkGrid workGridCSSClass={styles['work-grid']} openModal={openModal} />
               <WhyDev8X {...whyDev} />
-              {/* <a class="Button_button-wrapper__2Ps4h page_homepage-bottom__link__bpR9a" target="_self" href="/about">
-                <span class="Button_button__lQZdm Button_button--icon__Me_aL Button_button--bg-secondary__6flN1" style="transform: translateX(0%) translateY(0%) rotate(0deg) translateZ(0px);">
-                  About Us
-                  <svg xmlns="http://www.w3.org/2000/svg" width="14" height="13" fill="none" stroke-width="0.5" viewBox="0 0 14 13" class="" style="--width: 14; --height: 13;">
-                    <path fill="currentColor" stroke="currentColor" d="M1 5.816H.75v1.326h10.014l-4.008 3.907-.173.168.162.179.563.62.174.191.186-.18 5.506-5.37.184-.178-.184-.18L7.668.932l-.186-.18-.174.191-.563.62-.162.178.173.169 4.008 3.907H1Z" vector-effect="non-scaling-stroke"></path>
-                  </svg>
-                </span>
-              </a>
-              <a class="Button_button-wrapper__2Ps4h" href="/expertise/headless">
-                <span class="Button_button__lQZdm Button_button--icon__Me_aL Button_button--bg-secondary__6flN1 ExpertiseCard_expertise-card__button__DdE04" style="transform: translateX(0%) translateY(0%) rotate(0deg) translateZ(0px);">
-                  Read more
-                  <svg xmlns="http://www.w3.org/2000/svg" width="14" height="13" fill="none" stroke-width="0.5" viewBox="0 0 14 13" class="" style="--width: 14; --height: 13;">
-                    <path fill="currentColor" stroke="currentColor" d="M1 5.816H.75v1.326h10.014l-4.008 3.907-.173.168.162.179.563.62.174.191.186-.18 5.506-5.37.184-.178-.184-.18L7.668.932l-.186-.18-.174.191-.563.62-.162.178.173.169 4.008 3.907H1Z" vector-effect="non-scaling-stroke"></path>
-                  </svg>
-                </span>
-              </a>
-              <a class="Button_button-wrapper__2Ps4h" target="_blank" href="https://sussextaps.com.au">
-                <span class="Button_button__lQZdm Button_button--icon__Me_aL Button_button--bg-secondary__6flN1" style="transform: translateX(0%) translateY(0%) rotate(0deg) translateZ(0px);">
-                  Visit Website
-                  <svg xmlns="http://www.w3.org/2000/svg" width="14" height="13" fill="none" stroke-width="0.5" viewBox="0 0 14 13" class="" style="--width: 14; --height: 13;">
-                    <path fill="currentColor" stroke="currentColor" d="M1 5.816H.75v1.326h10.014l-4.008 3.907-.173.168.162.179.563.62.174.191.186-.18 5.506-5.37.184-.178-.184-.18L7.668.932l-.186-.18-.174.191-.563.62-.162.178.173.169 4.008 3.907H1Z" vector-effect="non-scaling-stroke"></path>
-                  </svg>
-                </span>
-              </a> */}
             </div>
           </main>
         </FooterRevealPageWrap>
