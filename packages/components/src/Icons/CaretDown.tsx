65e1f3df250eb62d006c64db3a91aba69912af26
ahmadsohaib <ahmadsohaib@dev8x.com>
2025-07-12 13:09:18 +0500

    Add new icons.

--- packages/components/src/Icons/CaretDown.tsx
+++ packages/components/src/Icons/CaretDown.tsx
@@ -1,3 +1,4 @@
+import { ChevronDownIcon } from '@heroicons/react/24/outline';
 import React from 'react';
 
 interface CaretDownProps {
@@ -8,11 +9,7 @@ interface CaretDownProps {
 }
 
 const CaretDown: React.FC<CaretDownProps> = ({ width = 16, height = 16, className, onClick }) => {
-  return (
-    <svg {...(width ? { width } : {})} {...(height ? { height } : {})} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" fill="currentColor" className={className}>
-      <path d="M137.4 374.6c12.5 12.5 32.8 12.5 45.3 0l128-128c9.2-9.2 11.9-22.9 6.9-34.9s-16.6-19.8-29.6-19.8L32 192c-12.9 0-24.6 7.8-29.6 19.8s-2.2 25.7 6.9 34.9l128 128z" />
-    </svg>
-  );
+  return <ChevronDownIcon {...(width ? { width } : {})} {...(height ? { height } : {})} className={className} />;
 };
 
 export default CaretDown;
