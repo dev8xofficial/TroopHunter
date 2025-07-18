62a15510f73a4f9aeb9531164a1464b88b601a57
ahmadsohaib <ahmadsohaib@dev8x.com>
2025-07-18 23:36:28 +0500

    Implement animations on all form modals

--- microfrontend/dev8X/components/Surfaces/SmoothModalWrapper/SmoothModalWrapper.tsx
+++ microfrontend/dev8X/components/Surfaces/SmoothModalWrapper/SmoothModalWrapper.tsx
@@ -5,6 +5,8 @@ import { openSmoothModalAtom, ModalType } from '../../../store/smoothModalAtom';
 import { createPortal } from 'react-dom';
 import SmoothModal from '@repo/components/src/Surfaces/SmoothModal/SmoothModal';
 import { useLenis } from '../../../hooks/LenisContext';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
 
 import ContactFormModalStyles from '../ContactFormModal/index.module.css';
 
@@ -39,10 +41,62 @@ const SmoothModalWrapper: React.FC<SmoothModalWrapperProps> = ({ modalType, togg
       content: modalInnerRef.current!
     });
 
+    if (typeof window !== 'undefined') {
+      gsap.registerPlugin(ScrollTrigger);
+    }
+
+    modalLenis.on('scroll', ScrollTrigger.update);
+
     return () => {
       document.body.classList.remove('overflow-hidden');
       modalLenis.destroy();
       lenis.start();
+    };
+  }, [isVisible]);
+
+  useEffect(() => {
+    if (!isVisible) return;
+    const element = document.querySelector('#modal-inner') as HTMLElement;
+    const modalInnerBg = document.querySelector('#modal-inner-bg') as HTMLElement;
+    const modal = document.querySelector('#modal') as HTMLElement;
+
+    if (element) {
+      gsap.to(element, {
+        '--progress': 1,
+        scrollTrigger: {
+          trigger: element,
+          start: 'top 20%',
+          end: 'top top',
+          scrub: true,
+          scroller: modalRef.current // <-- Add this!
+        }
+      });
+
+      gsap.to(modalInnerBg, {
+        borderRadius: '0px',
+        scrollTrigger: {
+          trigger: element,
+          start: 'top 20%',
+          end: 'top top',
+          scrub: true,
+          scroller: modalRef.current // <-- Add this!
+        }
+      });
+
+      gsap.to(modal, {
+        padding: '0px',
+        scrollTrigger: {
+          trigger: element,
+          start: 'top 20%',
+          end: 'top top',
+          scrub: true,
+          scroller: modalRef.current // <-- Add this!
+        }
+      });
+    }
+
+    return () => {
+      ScrollTrigger.killAll();
     };
   }, [isVisible]);
 
--- packages/components/src/Surfaces/SmoothModal/SmoothModal.tsx
+++ packages/components/src/Surfaces/SmoothModal/SmoothModal.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { CSSProperties, useEffect } from 'react';
 import { ModalCloseButton } from '@repo/components';
 
 import styles from './index.module.css';
@@ -18,12 +18,14 @@ const SmoothModal: React.FC<SmoothModalProps> = ({ toggle, children, modalRef, m
         <div className={styles['modal-backdrop']} style={{ opacity: 0.5, pointerEvents: 'all' }}></div>
         <div tabIndex={0}></div>
         <div className={styles['modal-tab-trap-start']} tabIndex={-1}></div>
-        <div className={`lenis lenis-smooth ${styles['modal']}`} style={{ opacity: 1 }} ref={modalRef}>
+        <div id="modal" className={`lenis lenis-smooth ${styles['modal']}`} style={{ opacity: 1 }} ref={modalRef}>
           <div className="lenis-content" ref={modalInnerRef}>
-            <div className={styles['modal-inner']} style={{ transform: 'none' }}>
-              <div className={`${styles['modal-inner__bg']} ${modalBGClassName}`} style={{ borderTopLeftRadius: '50px', borderTopRightRadius: '50px', left: '0px', right: '0px' }}></div>
+            <div id="modal-inner" className={styles['modal-inner']} style={{ '--progress': 0, transform: 'none' } as CSSProperties}>
+              <div id="modal-inner-bg" className={`${styles['modal-inner__bg']} ${modalBGClassName}`} style={{ borderTopLeftRadius: '50px', borderTopRightRadius: '50px', left: '0px', right: '0px' }}></div>
               <ModalCloseButton onClick={toggle} />
-              <main className={styles['modal-content']}>{children}</main>
+              <main id="modal-content" className={styles['modal-content']}>
+                {children}
+              </main>
             </div>
           </div>
         </div>
