f9bec4ba2551b40cefeaf4cf256f19a205b0f6b8
helloabdul <contact@helloabdul.com>
2025-10-24 02:11:05 +0500

    Setup microsoft clarity to record user sessions and get insights for user experiences.

--- microfrontend/dev8X/pages/_app.tsx
+++ microfrontend/dev8X/pages/_app.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Script from 'next/script';
 import { Provider } from 'jotai';
 import { LenisProvider } from '../hooks/LenisContext';
 
@@ -7,11 +8,26 @@ import '../styles/globals.css';
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+
+      <Script
+        id="microsoft-clarity"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+                  (function(c,l,a,r,i,t,y){
+                      c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                      t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                      y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+                  })(window, document, "clarity", "script", "turxdn6oca");
+                `
+        }}
+      />
+
       <LenisProvider>
         <Provider>
-          <Head>
-            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-          </Head>
           <Component {...pageProps} />
         </Provider>
       </LenisProvider>
