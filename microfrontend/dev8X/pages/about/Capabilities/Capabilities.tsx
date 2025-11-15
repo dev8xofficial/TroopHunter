26a0cb65e20c4ceeb9ba5b637a7b80240e915525
helloabdul <contact@helloabdul.com>
2025-11-16 00:51:26 +0500

    Remove capabilities from data folder in dev8x.

--- microfrontend/dev8X/pages/about/Capabilities/Capabilities.tsx
+++ microfrontend/dev8X/pages/about/Capabilities/Capabilities.tsx
@@ -7,16 +7,17 @@ const Capabilities: React.FC<{ capabilities: CapabilitiesContent }> = ({ capabil
   return (
     <>
       <div className={styles['about-capabilities__grid']}>
-        {capabilities.map(({ heading, items }) => (
-          <section className={styles['about-column']} style={{ opacity: 1, transform: 'translateX(0px)' }} key={heading}>
-            <h3 className={styles['about-column__heading']}>{heading}</h3>
-            <ul className={styles['about-column__list']}>
-              {items.map((item) => (
-                <li key={item}>{item}</li>
-              ))}
-            </ul>
-          </section>
-        ))}
+        {capabilities?.length > 0 &&
+          capabilities.map(({ heading, items }) => (
+            <section className={styles['about-column']} style={{ opacity: 1, transform: 'translateX(0px)' }} key={heading}>
+              <h3 className={styles['about-column__heading']}>{heading}</h3>
+              <ul className={styles['about-column__list']}>
+                {items.map((item) => (
+                  <li key={item}>{item}</li>
+                ))}
+              </ul>
+            </section>
+          ))}
       </div>
     </>
   );
