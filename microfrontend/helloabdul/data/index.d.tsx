7f574366c0941e188600891be11b80b3b70f2f28
ahmadsohaibdev8x <ahmadsohaibdev8x@gmail.com>
2025-11-28 23:04:04 +0500

    Remove the extra pages from both the dev8x & helloabdul.

--- microfrontend/helloabdul/data/index.d.tsx
+++ microfrontend/helloabdul/data/index.d.tsx
@@ -34,7 +34,7 @@ const PageData: HomeContent = {
     offices: [
       {
         country: 'Pakistan',
-        city: 'Lahore, Punjab (HQ)',
+        city: 'Lahore, Punjab',
         phone: '+92 (329) 294-7777'
       },
       {
