48c6988646c60774dd7ec634bb387195e1648002
helloabdul <contact@helloabdul.com>
2025-12-01 02:19:40 +0500

    Infrastructure Improvements: Added clarity configuration for helloabdul.

--- microfrontend/helloabdul/pages/_app.tsx
+++ microfrontend/helloabdul/pages/_app.tsx
@@ -29,7 +29,7 @@ export default function App({ Component, pageProps }) {
                         c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
                         t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
                         y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-                    })(window, document, "clarity", "script", "turxdn6oca");`
+                    })(window, document, "clarity", "script", "uedvnt67nj");`
           }}
         />
       )}
